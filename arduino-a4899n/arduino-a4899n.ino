11755a2e4ab7a10ddfcc114cb70f359b66b8bd19
julfadzlymuhaimin98 <57395950+Julfadzly98@users.noreply.github.com>
2022-10-05 22:09:40 +0800

    Add files via upload

---
+++ arduino-a4899n/arduino-a4899n.ino
@@ -0,0 +1,84 @@
+const int DIR = 12; //D6
+const int STEP = 14; //D5
+const int Ena = 2; //D4
+const int  steps_per_rev = 2000;
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(STEP, OUTPUT);
+  pinMode(DIR, OUTPUT);
+  pinMode(Ena, OUTPUT);
+}
+void loop()
+{
+  digitalWrite(DIR, HIGH);
+  Serial.println("Spinning Clockwise...");
+  
+  for(int i = 0; i< 200; i++)
+  {
+    digitalWrite(STEP, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(STEP, LOW);
+    delayMicroseconds(1000);
+  }
+  delay(2000);
+  digitalWrite(DIR, HIGH);
+  Serial.println("Spinning Clockwise...");
+  
+  for(int i = 0; i< 200; i++)
+  {
+    digitalWrite(STEP, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(STEP, LOW);
+    delayMicroseconds(1000);
+  }
+  delay(2000);
+  digitalWrite(DIR, HIGH);
+  Serial.println("Spinning Clockwise...");
+  
+  for(int i = 0; i< 200; i++)
+  {
+    digitalWrite(STEP, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(STEP, LOW);
+    delayMicroseconds(1000);
+  }
+  delay(2000);
+
+//////////////////////////////////bearer///////////////////////////////////////////////////////
+ 
+  digitalWrite(DIR, LOW);
+  Serial.println("Spinning Anti-Clockwise...");
+
+  for(int i = 0; i< 200; i++)
+  {
+    digitalWrite(STEP, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(STEP, LOW);
+    delayMicroseconds(1000);
+  }
+  delay(2000);
+  digitalWrite(DIR, LOW);
+  Serial.println("Spinning Anti-Clockwise...");
+
+  for(int i = 0; i< 200; i++)
+  {
+    digitalWrite(STEP, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(STEP, LOW);
+    delayMicroseconds(1000);
+  }
+  delay(2000);
+  digitalWrite(DIR, LOW);
+  Serial.println("Spinning Anti-Clockwise...");
+
+  for(int i = 0; i< 200; i++)
+  {
+    digitalWrite(STEP, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(STEP, LOW);
+    delayMicroseconds(1000);
+  }
+  delay(2000);
+}
