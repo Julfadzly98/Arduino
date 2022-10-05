a89a7e3dde0e46f33e3b5fe80f7c21b49a910676
julfadzlymuhaimin98 <57395950+Julfadzly98@users.noreply.github.com>
2022-10-05 22:08:39 +0800

    Add files via upload

---
+++ telearduino2/telearduino2.ino
@@ -0,0 +1,135 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/telegram-control-esp32-esp8266-nodemcu-outputs/
+  
+  Project created using Brian Lough's Universal Telegram Bot Library: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+  Example based on the Universal Arduino Telegram Bot Library: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot/blob/master/examples/ESP8266/FlashLED/FlashLED.ino
+*/
+
+#ifdef ESP32
+  #include <WiFi.h>
+#else
+  #include <ESP8266WiFi.h>
+#endif
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>   // Universal Telegram Bot Library written by Brian Lough: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+#include <ArduinoJson.h>
+
+// Replace with your network credentials
+const char* ssid = "smartspace";
+const char* password = "smartspacekk";
+
+// Initialize Telegram BOT
+#define BOTtoken "5663680281:AAG-Bnw4qWBdlRGvxgPGQoVx-p6Fb04JrBA"  // your Bot Token (Get from Botfather)
+
+// Use @myidbot to find out the chat ID of an individual or a group
+// Also note that you need to click "start" on a bot before it can
+// message you
+#define CHAT_ID "187740907"
+
+#ifdef ESP8266
+  X509List cert(TELEGRAM_CERTIFICATE_ROOT);
+#endif
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+// Checks for new messages every 1 second.
+int botRequestDelay = 1000;
+unsigned long lastTimeBotRan;
+
+int ledPin = 12;
+int ledPinn = 14;
+int inputPin = 13; // choose input pin (for Infrared sensor) 
+int val = 0; 
+bool ledState = LOW;
+
+// Handle what happens when you receive new messages
+void handleNewMessages(int numNewMessages) {
+  Serial.println("handleNewMessages");
+  Serial.println(String(numNewMessages));
+
+  for (int i=0; i<numNewMessages; i++) {
+    // Chat id of the requester
+    String chat_id = String(bot.messages[i].chat_id);
+    if (chat_id != CHAT_ID){
+      bot.sendMessage(chat_id, "Unauthorized user", "");
+      continue;
+    }
+    
+    // Print the received message
+    String text = bot.messages[i].text;
+    Serial.println(text);
+
+    String from_name = bot.messages[i].from_name;
+
+   // if (text == "/led_on") {
+     // bot.sendMessage(chat_id, "LED state set to ON", "");
+      //ledState = HIGH;
+      //digitalWrite(ledPin, ledState);
+    //}
+    
+    if (text == "/state") {
+      if (digitalRead(inputPin)){
+        bot.sendMessage(chat_id, "FULL", "");
+      }
+      else{
+        bot.sendMessage(chat_id, "AVAILABLE", "");
+      }
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  #ifdef ESP8266
+    configTime(0, 0, "pool.ntp.org");      // get UTC time via NTP
+    client.setTrustAnchors(&cert); // Add root certificate for api.telegram.org
+  #endif
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPinn, OUTPUT); 
+  pinMode(inputPin, INPUT); // declare Infrared sensor as input
+  digitalWrite(ledPin,ledState);
+  
+  // Connect to Wi-Fi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  #ifdef ESP32
+    client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+  #endif
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+
+   val = digitalRead(inputPin); // read input value 
+   if (val == HIGH)
+   { // check if the input is HIGH
+      digitalWrite(ledPin, LOW); // turn LED OFF
+      digitalWrite(ledPinn, HIGH);   
+   } 
+   else 
+   { 
+      digitalWrite(ledPin, HIGH); // turn LED ON
+      digitalWrite(ledPinn, LOW); 
+   } 
+
+  
+  if (millis() > lastTimeBotRan + botRequestDelay)  {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    while(numNewMessages) {
+      Serial.println("got response");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+    lastTimeBotRan = millis();
+  }
+}
